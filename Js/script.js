cadc8bae923115eaf2c0c9fb1c4850655efbf7d6
mathomsen <54878514+mathomsen@users.noreply.github.com>
2020-05-13 14:39:25 -0400

    FFT Visualization
    Added Sound Visualiztion!
    Also fixed pause/play bug

--- Js/script.js
+++ Js/script.js
@@ -1,13 +1,16 @@
+var audioPlayer = document.getElementById("audio-player");
+
 function PausePlay(el) {
     var el = el.getElementsByClassName('name')[0].innerHTML;
-    var audioPlayer = document.getElementById("audio-player");
     audioPlayer.style.visibility = "visible";
-    console.log("Sounds/" + el + ".wav")
-    console.log(audioPlayer.currentSrc.substring(35)); //This is a major source of bugs with sound player!!! fix pls!
-    if ((audioPlayer.paused === false) && (audioPlayer.currentSrc.substring(35) === el + ".wav")) {
+    var playerLength = audioPlayer.currentSrc.length;
+    var playerSource = audioPlayer.currentSrc.substring(playerLength - 14); //This is a major source of bugs with sound player!!! fix pls!
+    var currentEl = "Sounds/" + el + ".wav";
+    currentEl = currentEl.substring(currentEl.length - 14);
+    if ((audioPlayer.paused === false) && (playerSource == currentEl)) {
         audioPlayer.pause();
     }
-    else if ((audioPlayer.paused === true) && (audioPlayer.currentSrc.substring(35) === el + ".wav")){
+    else if ((audioPlayer.paused === true) && (playerSource == currentEl)){
         audioPlayer.play();
     }
     else {
@@ -221,4 +224,45 @@ function downloadZip() {
     .then(function(blob) {
     saveAs(blob, "Sounds.zip");
     });
+}
+
+
+//-----------SOUND VISUALIZATION/CANVAS!------------//
+
+//soundArray = [Actinium, Aluminum, Antimony, Argon, Arsenic, Barium, Beryllium, Bismuth, Boron, Cadmium, Caesium, Carbon, Cerium, Chromium, Cobalt, Copper, Dyprosium, Europium, Fluorine, Gadolinium, Gallium, Germanium, Hafnium, Helium, Holmium, Hydrogen, Indium, Iodine, Iridium, Iron, Krypton, Lanthanum, Magnesium, Manganese, Neodynium, Neon, Neptunium, Nickel, Niobium, Nitrogen, Oxygen, Palladium, Phosphorous, Plutonium, Potassium, Praseodymium, Promethium, Protactinium, Rhenium, Rhodium, Rubidium, Samarium, Scandium, Selenium, Silicon, Silver, Sodium, Sulfur, Tantalum, Technetium, Tellurium, Terbium, Thallium, Thorium, Thulium, Tin, Titanium, Uranium, Vanadium, Ytterbium, Yttrium, Zirconium];
+
+var canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height;
+
+window.onload = function() {
+  context = new AudioContext();
+  //audio.crossOrigin = "anonymous";
+    analyser = context.createAnalyser();
+    canvas = document.getElementById('analyser_render');
+    ctx = canvas.getContext('2d');
+    source = context.createMediaElementSource(audioPlayer);
+    source.connect(analyser);
+    analyser.connect(context.destination);
+    frameLooper();
+}
+
+window.addEventListener('click', function() {
+  context.resume().then(() => {
+    console.log('Playback resumed successfully');
+  });
+});
+
+
+function frameLooper() {
+    window.requestAnimationFrame(frameLooper);
+    fbc_array = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(fbc_array);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'black';
+    bars = 100;
+    for (var i = 0; i<bars; i++) {
+        bar_x = i * 3;
+        bar_width = 2;
+        bar_height = -(fbc_array[i] / 2);
+        ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
+    }
 }
